8130f9646fb329415e2648d1ec488dd36709491a
synleon <synleon@gmail.com>
2019-02-14 00:08:32 -0500

    stage commit

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,7 +1,7 @@
 package entity;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
